61be927d3ceee1c93bc35a903068325d66f4b5b5
Peter Verhas <peter@verhas.com>
2012-08-06 18:31:28 +0200

    modified to have Main-Class in the jar

---
+++ jscriptbasic/src/main/java/com/scriptbasic/main/CommandLine.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.scriptbasic.main;
+
+/**
+ * @author Peter Verhas
+ * @date Aug 6, 2012
+ * 
+ */
+public class CommandLine {
+    public static void main(String[] args) {
+        System.out.println("Hello world\n");
+    }
+}
