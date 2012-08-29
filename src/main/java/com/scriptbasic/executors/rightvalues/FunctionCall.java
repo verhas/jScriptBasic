2c291f7054e6d7402fba089054963a98be661eb5
Peter Verhas <peter@verhas.com>
2012-08-29 18:01:41 +0200

    do not just ignore the undefined function call

--- src/main/java/com/scriptbasic/executors/rightvalues/FunctionCall.java
+++ src/main/java/com/scriptbasic/executors/rightvalues/FunctionCall.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import com.scriptbasic.executors.AbstractIdentifieredExpressionListedExpression;
 import com.scriptbasic.executors.commands.CommandSub;
+import com.scriptbasic.interfaces.BasicRuntimeException;
 import com.scriptbasic.interfaces.ExecutionException;
 import com.scriptbasic.interfaces.Expression;
 import com.scriptbasic.interfaces.ExpressionList;
@@ -32,11 +33,13 @@ public class FunctionCall extends
 		List<RightValue> args = ExpressionUtility.evaluateExpressionList(
 				interpreter, getExpressionList());
 		Method method = interpreter.getJavaMethod(null, functionName);
-		if (method != null) {
+		if (method == null) {
+			throw new BasicRuntimeException("There is no callable function '"
+					+ functionName + "'");
+		} else {
 			Object methodResult = null;
 			methodResult = ReflectionUtility.invoke(functionName, interpreter,
 					method, null, args);
-
 			result = RightValueUtility.createRightValue(methodResult);
 		}
 		return result;
